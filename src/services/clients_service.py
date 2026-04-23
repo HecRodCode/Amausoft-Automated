c21db7054156c5e4fca07ebc2a2ef3e889ee1fb5
Hector Rios <riosrodriguezhectorhernan59@gmail.com>
2026-04-23 16:55:29 -0500

    feat: integration of a new service for get client data from API

---
+++ src/services/clients_service.py
@@ -0,0 +1,98 @@
+import asyncio
+import httpx
+import pandas as pd
+import os
+from datetime import datetime
+
+class ClientsService:
+  def __init__(self):
+    self.external_data = {'last_value': none, 'last_update': none}
+    self.url = 'https://randomuser.me/api/?results=100'
+    self.csv_path = 'data/clients_dataset.csv'
+    self.limit = 3000
+
+  def _get_current_count(self):
+    """ Returns the current number of clients """
+    if not os.path.exists(self.csv_path):
+      return 0
+
+    try:
+      df = pd.read_csv(self.csv_path)
+      return len(df)
+    except FileNotFoundError:
+      return 0
+
+  def _get_next_id(self):
+    """ Returns the next client id """
+    if not os.path.exists(self.csv_path):
+      return 1
+    try:
+      df = pd.read_csv(self.csv_path)
+      if df.empty: return 1
+      return int(df['client_id'].max() + 1)
+    except FileNotFoundError:
+      return 1
+
+  def _save_to_csv(self, json_data):
+    """ Save the current clients data to csv """
+    if not os.path.exists(self.csv_path):
+      try:
+        users = json_data.get('results', [])
+        current_id = self._get_current_count()
+        rows = []
+
+        # Loop for convert JSON data to csv
+        for user in users:
+          rows.append({
+            'client_id': current_id,
+            'name': f"{user['name']['first']} {user['name']['last']}".upper(),
+            "city": user['location']['city'].upper(),
+            "country": user['location']['country'].upper(),
+            "email": user['email'],
+            "fetch_timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+          })
+          current_id += 1
+
+        df_new = pd.DataFrame(rows)
+        file_exists = os.path.isfile(self.csv_path)
+        df_new.to_csv(
+          self.csv_path,
+          mode='a',
+          index=False,
+          header=not file_exists,
+          encoding='utf-8'
+        )
+        print(f"{len(rows)} Saved clients. Current total: {self._get_current_count()}")
+
+      except Exception as e:
+        print(f"Error CSV processing: {e}")
+
+      async def fetch_clients_periodically(self):
+        async with httpx.AsyncClient() as client:
+          while True:
+            current_total = self._get_current_count()
+
+            # Limit check
+            if current_total >= self.limit:
+              print(f"Limit the {self.limit} reached. Ingestion stopped.")
+              break
+
+            # Calculate how many are needed to avoid exceeding 3000
+            remaining = self.limit - current_total
+            fetch_n = min(100, remaining)
+
+            try:
+              params = {"results": fetch_n}
+              response = await client.get(self.url, params=params, timeout=10.0)
+
+              if response.status_code == 200:
+                data = response.json()
+                self.external_data["last_value"] = data
+                self.external_data["last_update"] = datetime.now().isoformat()
+                self._save_to_csv(data)
+            except Exception as e:
+              print(f"Connection error: {e}")
+
+            await asyncio.sleep(40)
+
+clients_service = ClientsService()
